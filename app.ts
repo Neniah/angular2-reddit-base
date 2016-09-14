415b90cd5a4c36a00ad17e25762c4c17fb1b74c9
Maria Lobillo <maria.lobillo.santos@gmail.com>
2016-09-14 18:27:57 +0200

    reddit-article

--- app.ts
+++ app.ts
@@ -4,35 +4,68 @@ import { BrowserModule } from '@angular/platform-browser';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 @Component({
-  selector: 'reddit',
+  selector: 'reddit-article',
+  host: {
+    class: 'row'
+  },
   template: `
-    <form action="" class="ui large form segment">
-      <h3 className="ui header">Add a Link</h3>
-
-      <div className="field">
-        <label htmlFor="title">Title:</label>
-        <input type="text" name="title" #newtitle>
+  <div className="four wide column center aligned votes">
+    <div className="ui statistic">
+      <div className="value">
+        {{ votes }}
       </div>
-
-      <div className="field">
-        <label htmlFor="link">Link:</label>
-        <input type="text" name="link" #newlink>
+      <div className="label">
+        Points
       </div>
-      <button (click)="addArticle(newtitle, newlink)"
-          class="ui positive right floated button">
-          Submit Link
-      </button>
-    </form>
+    </div>
+  </div>
+  <div className="twelve wide column">
+    <a href="{{ link }}" class="ui large header">{{ title }}</a>
+    <ul class="ui big horizontal list voters">
+      <li class="item">
+        <a href (click)="voteUp()">
+          <i class="arrow up icon"></i>
+            upvote
+        </a>
+      </li>
+      <li class="item">
+        <a href (click)="voteDown()">
+          <i class="arrow down icon"></i>
+          downvote
+        </a>
+      </li>
+    </ul>
+  </div>
   `
 })
 
-class RedditApp {
+/*class RedditApp {
   addArticle(title: HTMLInputElement, link: HTMLInputElement): boolean {
     console.log(`Adding article title: ${title.value} and link: ${link.value}`);
     return false;
   }
 }
+*/
 
+class ArticleComponente{
+  votes: number
+  title: string;
+  link: string;
+
+  constructor(){
+    this.title = 'Angular 2';
+    this.link = 'http://angular.io';
+    this.votes = 10;
+  }
+
+  voteUp(){
+    this.votes += 1;
+  }
+
+  voteDown(){
+    
+  }
+}
 @NgModule({
   declarations: [ RedditApp ],
   imports: [ BrowserModule ],
